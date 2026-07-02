1e142ec2b69bca0f26c9dd7990016d32e255077b
Leandro <leandrodanteciriaco@gmail.com>
2026-07-02 11:50:48 -0300

    se realizo los msj de error en formularios de autos, motos y ventas, sin probar porque no puedo

--- src/app/Pages/Vehiculo/form-vehiculo/moto-form/moto-form.ts
+++ src/app/Pages/Vehiculo/form-vehiculo/moto-form/moto-form.ts
@@ -153,6 +153,11 @@ export class MotoFormComponent {
   } 
 
   onSubmit(){
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+
     if(this.isEditable){
       this.editarMoto()
     }else{
